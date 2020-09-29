1ad5e0988af5352408d754bdccccc88bc18377bd
Даниил Косачев <kdaniil@MCS>
2020-09-29 13:32:48 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace GlobalEventsHandling
+{
+    public partial class Form1 : Form
+    {
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        readonly IKeyboardMouseEvents GlobalHook;
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        public Form1()
+        {
+            this.GlobalHook = Hook.GlobalEvents();
+            this.GlobalHook.MouseClick += WriteClickCoordinates;
+
+            InitializeComponent();
+        }
+
+        private void WriteClickCoordinates(object sender, MouseEventArgs e)
+        {
+            var color = GetColorAt(e.Location);
+            label1.Text = $"{color.ToArgb():X}";
+        }
+
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+    }
+}
